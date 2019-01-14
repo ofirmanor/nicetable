929575e05188117280799d4d0961a241cf48ca1f
ofirmanor <ofir.manor@gmail.com>
2019-01-14 15:07:35 +0200

    merged aux_functions.py into nicetable.py

--- nicetable/nicetable.py
+++ nicetable/nicetable.py
@@ -1,7 +1,15 @@
 import numbers
 from typing import List, Union, Optional, Callable, Any, Tuple
 
-from nicetable.aux_functions import *
+
+def coalesce(*args: Any) -> Any:
+    """ Return the first non-None argument."""
+    return next((x for x in args if x is not None), None)
+
+
+def non_printable_to_space(s: str) -> str:
+    """ Return the input string, with non-printable characters replaced with a space (Unicode-friendly) """
+    return s  # TODO implement and double-check
 
 
 class NiceTable:
