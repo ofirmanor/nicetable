050e07d973a0b4a88d0632f236a1dd43fdd6ae19
ofirmanor <ofir.manor@gmail.com>
2019-01-14 15:40:57 +0200

    version 0.6 - text wrapping, set_col_options()

--- nicetable/examples.py
+++ nicetable/examples.py
@@ -40,18 +40,21 @@ for layout in MyNiceTable.builtin_layouts():
     out.append(layout)
 print(out)
 
-# Example: setting a column function
-out = NiceTable(['Name', 'Type', 'Height(cm)', ' Weight(kg)'], layout='default')
+# Example: setting column-level options
+out = NiceTable(['Name', 'Type', 'Height(cm)', ' Weight(kg)'])
 for pokemon in json.loads(NiceTable.SAMPLE_JSON):
     out.append([pokemon['name'], pokemon['type'], pokemon['height'], pokemon['weight']])
-out.set_col_func(0, lambda x: x.upper())
-out.set_col_func('Type', lambda x: x.lower() if x != 'Electric' else None)
-print(out)
 
+# set column options by position
+out.set_col_options(0, adjust='center')
+# set column options by column name
+out.set_col_options('Type',
+                    func=lambda x: x.lower() if x != 'Electric' else None,
+                    none_string = 'N/A')
 
 # Example: different numeric alignments
 out = NiceTable(['standard left', 'standard center', 'standard right', 'strict_left', 'strict_center', 'strict_right'])
-n_list = [6.901, 6,1, 122]
+n_list = [6.901, 6.1, 122]
 [out.append([n] * 6) for n in n_list]
 out.col_adjust = ['left', 'center', 'right', 'strict_left', 'strict_center', 'strict_right']
 print(out)
@@ -67,9 +70,11 @@ out.value_too_long_policy = 'truncate'
 print(out)
 
 # Example: newlines
-out = NiceTable(['Code', 'Product Description\n(Long)'])
-out.append([1, 'Boeing 777\nBatteries not included.\nMay contain nuts.'])
-out.append([2, 'Sack of sand'])
+out = NiceTable(['Code', 'Product Description\n(Long)']) \
+    .append([1, 'Boeing 777\nBatteries not included.\nMay contain nuts.']) \
+    .append([2, 'Sack of sand'])
 print(out)
 out.value_newline_replace = '\\n'
 print(out)
+
+
